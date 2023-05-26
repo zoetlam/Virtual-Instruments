d09e455f08e23981680d38cdbe2af732ff166d26
Thi Lam <118875553+zoetlam@users.noreply.github.com>
2023-05-26 08:55:32 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 let arr = ['crash', 'kick-bass', 'snare', 'tom-1', 'tom-2', 'tom-3', 'tom-4'];
 let arrLetters = ['w','a','s','d','j','k','l'];
+function makeSound(value){
+    let sound = new Audio('sounds/'+ arr[value] + '.mp3');
+    sound.play();
+}
 
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++){   
-        let audio = new Audio('sounds/'+ arr[i] + '.mp3');
-        document.querySelectorAll('.drum')[i].addEventListener('click',function () {audio.play();});  
+        document.querySelectorAll('.drum')[i].addEventListener('click',function () {makeSound(i);});  
 }
 document.addEventListener('keydown', function(event){
-    let num = arrLetters.indexOf(event.key);
-    let sound = new Audio('sounds/'+ arr[num] + '.mp3');
-    sound.play();
+    makeSound(arrLetters.indexOf(event.key));
 })
+
 
 
 
